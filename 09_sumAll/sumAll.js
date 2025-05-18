0fe76a0120c4ceca8f93b36ff4a88a1c059221a6
Victor <johnjohnjohnnyjoe@gmail.com>
2025-05-18 19:09:59 +0800

    Add solution to JS exercises 06 to 11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof(a) != "number" || typeof(b) != "number") {
+        return "ERROR";
+    } else if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    else if (Math.floor(a) - a != 0 || Math.floor(b) - b != 0) {
+        return "ERROR";
+    };
 
+    let start;
+    let end;
+
+    if (a < b) {
+        start = a;
+        end = b;
+    } else {
+        start = b;
+        end = a;
+    };
+
+    let sum = 0;
+        for (let i = start; i <= end; i++) {
+        sum += i};
+    return sum
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
